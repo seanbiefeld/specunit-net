0efee7d4b242379ada3c5e11b53d84a5538fd1c6
sbellware <sbellware@943d5fe6-fc41-0410-b744-898b730eeab7>
2008-01-03 05:59:45 +0000

    Added support for version stamping with the SVN revision number

--- src/SpecUnit/Properties/AssemblyInfo.cs
+++ src/SpecUnit/Properties/AssemblyInfo.cs
@@ -8,9 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SpecUnit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Scott Bellware")]
-[assembly: AssemblyProduct("SpecUnit")]
-[assembly: AssemblyCopyright("Copyright © Scott Bellware 2007")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +18,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("91201aaf-496d-481c-af19-54aef133452f")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
