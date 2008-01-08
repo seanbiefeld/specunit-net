a6b12c716f742b989a90b606076bbdfd2a73e66b
sbellware <sbellware@943d5fe6-fc41-0410-b744-898b730eeab7>
2008-01-08 07:42:48 +0000

    Added "hehaves like" to Context

--- src/SpecUnit.Specs.AssemblyUnderTest/Context_with_concern.cs
+++ src/SpecUnit.Specs.AssemblyUnderTest/Context_with_concern.cs
@@ -2,45 +2,63 @@ using NUnit.Framework;
 
 namespace SpecUnit.Specs.AssemblyUnderTest
 {
-    [Concern(typeof(SomeConcern))]
-    public class Context_with_concern
-    {
-        [Test]
-        public void should_jump()
-        {
+	[Concern(typeof(SomeConcern))]
+	public class Context_with_concern
+	{
+		[Test]
+		public void should_jump()
+		{
 
-        }
+		}
 
-        [Test]
-        public void should_jump_when_I_say_how_high()
-        {
+		[Test]
+		public void should_jump_when_I_say_how_high()
+		{
 
-        }
+		}
 
-        [Test]
-        public void should_jump_if_I_say_how_high()
-        {
+		[Test]
+		public void should_jump_if_I_say_how_high()
+		{
 
-        }
-    }
+		}
+	}
 
-    [Concern(typeof(SomeConcern))]
-    public class Context_with_same_concern
-    {
-        [Test]
-        public void should_quack()
-        {
+	[Concern(typeof(SomeConcern))]
+	public class Context_with_same_concern
+	{
+		[Test]
+		public void should_quack()
+		{
 
-        }
-    }
+		}
+	}
 
-    [Concern(typeof(SomeOtherConcern))]
-    public class Context_with_some_other_concern
-    {
-        [Test]
-        public void should_walk()
-        {
+	[Concern(typeof(SomeOtherConcern))]
+	public class Context_with_some_other_concern
+	{
+		[Test]
+		public void should_walk()
+		{
 
-        }
-    }
+		}
+	}
+
+	[Concern(typeof(SomeConcern))]
+	public class Context_with_behaves_like : behaves_like_a_common_context
+	{
+	}
+
+	[Concern(typeof(SomeConcern))]
+	public class Context_with_subclass : SomeSubclass
+	{
+	}
+
+	public class behaves_like_a_common_context
+	{
+	}
+
+	public class SomeSubclass
+	{
+	}
 }
